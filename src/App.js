3bf6be8e515a613edc509ccc3dd025c5a345a354
Oudani Achraf <achraf.oudani@gmail.com>
2021-02-09 20:39:57 +0100

    get init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./Component/Header";
+import { Balance } from "./Component/Balance";
+import { IncomeExpenses } from "./Component/IncomeExpenses";
+import { TransactionList } from "./Component/TransactionList";
+import { AddNewTransaction } from "./Component/AddNewTransaction";
+import { GlobalProvider } from "./context/GlobaleState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddNewTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
